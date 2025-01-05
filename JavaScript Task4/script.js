1e6b032ee46cd78d532a6c7f76b65ba18de1470b
Nada Kdesat <nadaqdesat@gmail.com>
2025-01-06 00:20:39 +0300

    JavaScript Task4

---
+++ JavaScript Task4/script.js
@@ -0,0 +1,15 @@
+// const menuIcon = document.getElementById('menu-icon');
+// const sidebar = document.getElementById('sidebar');
+// const overlay = document.getElementById('overlay');
+
+// // عند النقر على أيقونة القائمة
+// menuIcon.addEventListener('click', () => {
+//   sidebar.style.left = sidebar.style.left === '0px' ? '-250px' : '0px';
+//   overlay.classList.toggle('active');
+// });
+
+// // عند النقر على الخلفية
+// overlay.addEventListener('click', () => {
+//   sidebar.style.left = '-250px';
+//   overlay.classList.remove('active');
+// });
